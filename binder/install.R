795968351800130d15d077af7e5533e4e3f15e95
G-Brennan <g.l.brennan@bangor.ac.uk>
2019-10-30 12:25:54 +0100

    add packages for analysis

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,3 @@
-pkgs = c("reshape2", "plyr", "readr", "ggplot2", "RColorBrewer","knitr", "tidyr")
+pkgs = c("reshape2", "plyr", "readr", "ggplot2", "RColorBrewer","knitr", "tidyr", "mvabund", "vcd", "car", "fitdistrplus")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
